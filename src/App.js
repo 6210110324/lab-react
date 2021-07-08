cd7dc32f89879a0f2a57e2c38f22d98c2f3b45ed
6210110324 <6210110324@psu.ac.th>
2021-07-08 10:44:37 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
 const word = "Hello";
 function App() {
   return (
     <div>
-      {
-        <WordCard value="hello"/>
-      }
+    <WordCard value={word}/>
     </div>
-  );
+    );
+   
 }
 
 export default App;
